1bb544d3bd8cb7b7add5e668193010a0f3f91179
wesley perez <wesley.m.perez@gmail.com>
2019-12-23 17:24:57 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,22 +2,29 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => "Lord Montague"
+        :name => "Lord Montague",
         :age => "53"
         #your key/value pairs here
         },
       :matriarch => {
-        :name => "Lady Montague"
+        :name => "Lady Montague",
         :age => "54"
         #your key/value pairs here
         },
       :hero => {
-        :name => "Romeo"
-        :age => "15"
+        :name => "Romeo",
+        :age => "15",
         :status => "alive"
         #your key/value pairs here
         },
-      :hero_friends => []
+      :hero_friends => [{
+        :name => "Benvolio",
+        :age => "17",
+        :attitude => "worried"
+        },{
+          :name => "Mercutio"
+          :age => "18"
+          :attitude => "hot-headed"}]
    },
    :capulet => {
       :patriarch => {
@@ -36,7 +43,13 @@ def third_challenge
         :status => "alive"
         #your key/value pairs here
         },
-      :heroine_friends => []
+      :heroine_friends => [{
+        :name => "Steven",
+        :age => "30",
+        :attitude => "confused" },{
+          :name => "Nurse",
+          :age => "44",
+          :attitude => "worried"} ]
    }
   }
 
